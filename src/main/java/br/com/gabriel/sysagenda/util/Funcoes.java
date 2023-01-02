2f58dae96eaca501bcfd45973117c296b263e6fc
Gabriel Amaral de Oliveira <gabrielamaral7575@gmail.com>
2023-01-02 14:57:22 -0300

    Adicionei a função de transformar date em string e string em date

--- src/main/java/br/com/gabriel/sysagenda/util/Funcoes.java
+++ src/main/java/br/com/gabriel/sysagenda/util/Funcoes.java
@@ -1,7 +1,11 @@
 package br.com.gabriel.sysagenda.util;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Funcoes {
-	
+
 	public static boolean verificaNome(String nome) {
 		boolean sn = false;
 		for (int i = 0; i < nome.length(); i++) {
@@ -28,5 +32,21 @@ public class Funcoes {
 	public static String[] separa(String palavra, String caractere) {
 		String[] palavras = palavra.split(caractere);
 		return palavras;
+	}
+
+	public static Date strToDate(String sData) {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		Date data = null;
+		try {
+			data = sdf.parse(sData);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return data;
+	}
+
+	public static String dateToStr(Date data) {
+
+		return new SimpleDateFormat("dd/MM/yyyy HH:mm").format(data);
 	}
 }
